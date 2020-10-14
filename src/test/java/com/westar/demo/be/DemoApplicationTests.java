c7029d8b3aba0ab4a50851bdd6233a3a7cd27677
cyrus2046 <ngkong@gmail.com>
2020-10-14 15:18:07 +0800

    Initial commit

---
+++ src/test/java/com/westar/demo/be/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.westar.demo.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
